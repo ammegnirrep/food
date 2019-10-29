c99f15ec711faae93b34489ae5996f8ed7e058ad
Temporary User <temporaryuser@Temporarys-MacBook-Pro.local>
2019-10-29 16:23:49 +0000

    fixed edit button bug

--- static/custom.js
+++ static/custom.js
@@ -1,48 +1,56 @@
-$.datetimepicker.setDateFormatter({
-    parseDate: function (date, format) {
-        var d = moment(date, format);
-        return d.isValid() ? d.toDate() : false;
-    },
-    formatDate: function (date, format) {
-        return moment(date).format(format);
-    },
-});
 
-$('.datetime').datetimepicker({
-    format:'DD-MM-YYYY hh:mm A',
-    formatTime:'hh:mm A',
-    formatDate:'DD-MM-YYYY',
-    useCurrent: false,
-});
+  $.datetimepicker.setDateFormatter({
+      parseDate: function (date, format) {
+          var d = moment(date, format);
+          return d.isValid() ? d.toDate() : false;
+      },
+      formatDate: function (date, format) {
+          return moment(date).format(format);
+      },
+  });
 
-// Initialise Pusher
-const pusher = new Pusher('b4ab28bc8116e3dceba1', {
-    cluster: 'eu',
-    encrypted: true
-});
+  $('.datetime').datetimepicker({
+      format:'DD-MM-YYYY hh:mm A',
+      formatTime:'hh:mm A',
+      formatDate:'DD-MM-YYYY',
+      useCurrent: false,
+  });
 
-var channel = pusher.subscribe('table');
+  // Initialise Pusher
+  const pusher = new Pusher('b4ab28bc8116e3dceba1', {
+      cluster: 'eu',
+      encrypted: true
+  });
 
-channel.bind('new-record', (data) => {
+  var channel = pusher.subscribe('table');
 
-    const expiry_date = moment(`${data.data.expiry_date}`, 'DD/MM/YYYY hh:mm a').format('YYYY-MM-DD hh:mm:ss a')
-   $('#flights').append(`
-        <tr id="${data.data.id}">
-            <th scope="row"> ${data.data.name} </th>
-            <td> ${data.data.quantity} </td>
-            <td> ${expiry_date} </td>
-        </tr>
-   `)
-});
+  channel.bind('new-record', (data) => {
 
-channel.bind('update-record', (data) => {
+      const expiry_date = moment(`${data.data.expiry_date}`, 'DD/MM/YYYY hh:mm a').format('YYYY-MM-DD hh:mm:ss a')
+     $('#flights').append(`
+          <tr id="${data.data.id}">
+              <th scope="row"> ${data.data.name} </th>
+              <td> ${data.data.quantity} </td>
+              <td> ${expiry_date} </td>
+          </tr>
+     `)
+  });
 
-    const expiry_date = moment(`${data.data.expiry_date}`, 'DD/MM/YYYY hh:mm a').format('YYYY-MM-DD hh:mm:ss a')
+  channel.bind('update-record', (data) => {
 
-    $(`#${data.data.id}`).html(`
-        <th scope="row"> ${data.data.name} </th>
-        <td> ${data.data.quantity} </td>
-        <td> ${expiry_date} </td>
-    `)
-
- });
+      const expiry_date = moment(`${data.data.expiry_date}`, 'DD/MM/YYYY hh:mm a').format('YYYY-MM-DD hh:mm:ss a')
+      if (includeEditButton) {
+      $(`#${data.data.id}`).html(`
+          <th scope="row"> ${data.data.name} </th>
+          <td> ${data.data.quantity} </td>
+          <td> ${expiry_date} </td>
+          <td> <a href="/edit/${ data.data.id }">Edit</a> </td>
+      `)
+    } else {
+      $(`#${data.data.id}`).html(`
+          <th scope="row"> ${data.data.name} </th>
+          <td> ${data.data.quantity} </td>
+          <td> ${expiry_date} </td>
+      `)
+    }
+   });

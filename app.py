e71795b537a86ac5e91b8d98699d860f5d638989
Temporary User <temporaryuser@Temporarys-MacBook-Pro.local>
2019-10-30 15:24:34 +0000

    break it maybe

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ pusher_client = pusher.Pusher(
     cluster=os.getenv('PUSHER_CLUSTER'),
     ssl=True)
 
+
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
